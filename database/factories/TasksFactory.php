e874a2358e051f0b84c6e650c74fc7a852f2078e
ahmedkhaeld <ahmed.alsaeidy.711@gmail.com>
2024-08-17 02:41:26 +0300

    feat add Tasks Model, migration, Factories
    run
     ./vendor/bin/sail php artisan make:model
    name model Tasks with full options except DatabaseSeeder
    
    1- modify the table structure at the migration
    2- at the TasksFactory map col names with values
    3- update the DatabaseSeeder to create fake data
    
    run the migration  up with seeding the factory
     ./vendor/bin/sail php artisan migrate --seed

---
+++ database/factories/TasksFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tasks>
+ */
+class TasksFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //map the column names with values
+            'name'=> fake()->sentence(),
+            'is_completed'=> rand(0, 1),
+        ];
+    }
+}
